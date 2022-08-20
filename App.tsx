eacbe13dc2354393aa3dcfb593b40664cab5bce0
mohandand <mohandand@gmail.com>
2022-08-20 13:05:43 -0700

    3 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 import axios from 'axios';
 import Posts from './components/Posts.js';
+import Pagination from './components/Pagination.js';
 
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(9);
+  const [postsPerPage] = useState(10);
 
   const fetchPosts = async () => {
     setLoading(true);
@@ -17,13 +18,18 @@ export default function App() {
     setLoading(false);
   };
 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   useEffect(() => {
     fetchPosts();
   }, []);
 
   return (
     <div>
-      <Posts posts={posts} losding={loading} />
+      <Posts posts={currentPosts} losding={loading} />
+      <Pagination totalPosts={posts.length} postsPerPage={postsPerPage} />
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './posts.css';
 
 export default function Posts({ posts, loading }) {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <div className="container">
       {posts.map((post, index) => {
